db2c500ca75c1104ce1a443cd94f77c51b22e97a
Bealonzo <alonzo.briane@gmail.com>
2019-05-18 22:42:01 -0400

    assignment-2976

---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js")
+
+//'Hello'
+greetings.greetingEnglish();
+
+//'Konichiwa'
+greetings.greetingJapanese();
+
+//"hola"
+greetings.greetingSpanish();
+
+//"Halló"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+goodbyes.goodbyeEnglish();
+
+//"Sayonara"
+goodbyes.goodbyeJapanese();
+
+//"Adois"
+goodbyes.goodbyeSpanish();
+
+//"Bless"
+goodbyes.goodbyeIcelandic();
